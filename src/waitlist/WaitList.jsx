0f5932d27fae31512e6e954f1c28fe44076b92a2
Marvywavy <adebayotitilayo02@gmail.com>
2025-07-31 10:58:07 +0100

    image fix

--- src/waitlist/WaitList.jsx
+++ src/waitlist/WaitList.jsx
@@ -66,13 +66,13 @@ function WaitList() {
                         <img
                         src={bus}
                         alt="Resqx fuel Tank"
-                        className="w-full h-[calc(100vh-64px)] opacity-60  mask-gradient "
+                        className="w-full h-[calc(100vh-64px)] object-cover opacity-60  mask-gradient "
                         />
                     </div>
                 </div>
 
 
-                <div className="absolute inset-0 lg:ml-[70px] lg:mt-[20px]  justify-center overflow-x-hidden " >
+                <div className="absolute inset-0 lg:ml-[70px] lg:mt-[20px] flex flex-col justify-start overflow-x-hidden " >
                     <div className="w-50 mt-5 mx-7">
                         <img src={logo} alt="Resqx logo " />
                     </div>
@@ -83,17 +83,17 @@ function WaitList() {
                         <div className="flex  flex-col sm:w:full md:w-[80%]  lg:w-full gap-5 m-0">
                             <div className="w-full">
                                 <p
-                                    className=" font-semibold text-[30px] leading-[100%] lg:w-[70%] "
+                                    className=" font-semibold text-[35px] leading-[100%] lg:w-[70%] "
                                 >Fuel Delivered. Anytime, Anywhere.</p>
                             </div>
 
                             <div className="leading-[30px] w-full  ">
                                 <p >
-                                    Join the ResQ-X Waitlist 
+                                    Join the Refuel by ResQ-X Waitlist
                                 </p>
                                 <p className="w-[80%]">
-                                    Skipthe fuel station. Get fuel delivered wherever you 
-                                    are — instantly, later, or when it’s urgent.
+                                    Skip the fuel station. 
+                                    Get fuel delivered wherever you are instantly, later or when it’s urgent.
                                 </p>     
                             </div>
 
@@ -103,8 +103,8 @@ function WaitList() {
                                         <path d="M10.192 0.279976L0.231997 10.528C0.0639974 10.696 0.183998 10.96 0.399998 10.96H6.16C6.352 10.96 6.472 11.152 6.4 11.32L2.776 19.456C2.656 19.72 2.992 19.936 3.184 19.72L13.696 7.81598C13.84 7.64798 13.72 7.38398 13.504 7.38398H7.408C7.216 7.38398 7.096 7.19198 7.192 7.02398L10.576 0.567976C10.72 0.327976 10.384 0.087976 10.192 0.279976Z" fill="#FF8500"/>
                                     </svg>
                                     <div >
-                                        <p>On-Demand Delivery</p>
-                                        <p className="leading-[20px] text-[12px] font-normal">
+                                        <p className="text-[14px]">On-Demand Delivery</p>
+                                        <p className="leading-[20px] text-[10px] font-normal">
                                             Fuel in Minutes
                                         </p>
                                     </div>
@@ -116,8 +116,8 @@ function WaitList() {
                                     </svg>
 
                                     <div >
-                                        <p>Scheduled & Recurring</p>
-                                        <p className="leading-[20px] text-[12px] font-normal">
+                                        <p className="text-[14px]">Scheduled & Recurring</p>
+                                        <p className="leading-[20px] text-[10px] font-normal">
                                             Plan ahead
                                         </p>
                                     </div>
@@ -143,9 +143,9 @@ function WaitList() {
 
 
                                     <div >
-                                        <p>No Hidden Charges </p>
-                                        <p className="leading-[20px] text-[12px] font-normal">
-                                            Transparent pricing
+                                        <p className="text-[14px]">Top-Quality Fuel Always </p>
+                                        <p className="leading-[20px] text-[10px] font-normal">
+                                            Trusted, safe for your engine.
                                         </p>
                                     </div>
                                 </div>
@@ -168,8 +168,8 @@ function WaitList() {
                                         </defs>
                                     </svg>
                                     <div >
-                                        <p>Emergency Refills</p>
-                                        <p className="leading-[20px] text-[12px] font-normal">
+                                        <p className="text-[14px]">Emergency Refills</p>
+                                        <p className="leading-[20px] text-[10px] font-normal">
                                             Fast refills for urgent needs
                                         </p>
                                     </div>
@@ -187,15 +187,15 @@ function WaitList() {
                                 <div className=" flex w-[80%] justify-between">
                                     <p
                                         className="border border-[#FF8500] bg-[#FF85002B] rounded-full  leading-[20px] text-[14px] font-normal py-3 px-5 bg"
-                                    >Everyday Drivers</p>
-
-                                    <p
-                                        className="border border-[#FF8500] bg-[#FF85002B] rounded-full  leading-[20px] text-[14px] font-normal py-3 px-5 bg"
-                                    >Busy Professionals</p>
-
-                                    <p
-                                        className="border border-[#FF8500] bg-[#FF85002B] rounded-full  leading-[20px] text-[14px] font-normal py-3 px-5 bg"
                                     >Fleets and Business</p>
+
+                                    <p
+                                        className="border border-[#FF8500] bg-[#FF85002B] rounded-full  leading-[20px] text-[14px] font-normal py-3 px-5 bg"
+                                    >Everyday Drivers </p>
+
+                                    <p
+                                        className="border border-[#FF8500] bg-[#FF85002B] rounded-full  leading-[20px] text-[14px] font-normal py-3 px-5 bg"
+                                    >Busy Professionals </p>
                                 </div>
                             </div>
                         </div>
@@ -204,16 +204,16 @@ function WaitList() {
 
                         <div className="bg-white sm:w-full md:w-[80%] lg:w-[80%] border border-[#FF8500] border-[] rounded-4xl p-7 text-[#777777] flex flex-col items-center gap-5">
                             <div className="w-full flex flex-col items-center justify-center gap-3">
-                                <p className="font-semibold text-[30px] leading-[100%] w-[90%] text-center">
-                                    Be the first to fuel smarter with ResQ-X and get Early Access
+                                <p className="font-semibold text-[30px] leading-[100%] w-[80%] text-center">
+                                    Be the first to fuel smarter with Refuel by ResQ-X 
                                 </p>
 
-                                <p className="w-[90%] text-center text-[18px] leading-[26px] tracking-[0.02em]">
-                                    ResQ-X delivers fuel straight to your vehicle at home, at work, or on the road. Anytime you need it.
+                                <p className="w-[80%] text-center text-[18px] leading-[26px] tracking-[0.02em]">
+                                    ResQ-X delivers fuel straight to your vehicle  at home, at work or on the road. Anytime you need it.
                                 </p>
                             </div>
 
-                            <form id="waitlistForm" className="w-[90%] flex flex-col items-center justify-center gap-4">
+                            <form id="waitlistForm" className="w-[80%] flex flex-col items-center justify-center gap-4">
                                 <input 
                                     type="text" 
                                     name="name"
