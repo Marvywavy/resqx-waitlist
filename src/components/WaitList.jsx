e18287dc457b1decb5fc28710387eb6564e3386b
Marvywavy <adebayotitilayo02@gmail.com>
2025-08-11 10:51:19 +0100

    popup add

--- src/components/WaitList.jsx
+++ src/components/WaitList.jsx
@@ -445,7 +445,7 @@ function WaitList() {
 
                             {showPopup && (
                                 <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-                                    <div className="relative bg-white px-6 py-4 rounded-[30px] shadow-lg text-center w-[362px] h-[270px] flex flex-col justify-center ">
+                                    <div className="relative bg-white px-6 py-4 rounded-[30px] shadow-lg text-center w-[270px] h-[270px] lg:w-[362px] lg:h-[270px] flex flex-col justify-center ">
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
                                             className="absolute right-[20px] top-[30px] -translate-y-1/2 cursor-pointer"
                                                 onClick={() => {
